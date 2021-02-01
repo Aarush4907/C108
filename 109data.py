cb7f173d1427f9a3090ed5e99f88a06efa9c85d7
Aarush4907 <76614253+Aarush4907@users.noreply.github.com>
2021-02-01 17:47:04 +0530

    Add files via upload

---
+++ 109data.py
@@ -0,0 +1,7 @@
+import plotly.express as px
+import plotly.figure_factory as ff
+import pandas as pd
+import csv
+df = pd.read_csv("D:\PROGRAMS\PYTHON\C102\data.csv")
+fig = ff.create_distplot([df["Weight(Pounds)"].tolist()],["Weight"],show_hist=False)
+fig.show()
